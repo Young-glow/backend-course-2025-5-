11d06483e9d794d2bd2d59ed0c02e2f1a05db51f
Anastasia web2025-1 <Anastasiia.Zhmurko@lnu.edu.ua>
2026-04-22 17:13:59 +0300

    4

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { program } = require('commander');
+const http = require('node:http');
 const fs = require('node:fs/promises');
+const path = require('node:path');
+const { program } = require('commander');
 
 program
   .requiredOption('-h, --host <address>', 'server address')
@@ -9,6 +11,25 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
+const server = http.createServer(async (req, res) => {
+  const statusCode = req.url.slice(1);
+  const cachePath = path.join(options.cache, `${statusCode}.jpg`);
+
+  if (req.method === 'GET') {
+    try {
+      const data = await fs.readFile(cachePath);
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    } catch {
+      res.writeHead(404);
+      res.end('Not Found in Cache');
+    }
+  } else {
+    res.writeHead(405);
+    res.end('Method Not Allowed');
+  }
+});
+
 async function initCache() {
   try {
     await fs.access(options.cache);
@@ -16,4 +37,9 @@ async function initCache() {
     await fs.mkdir(options.cache, { recursive: true });
   }
 }
-initCache();
+
+initCache().then(() => {
+  server.listen(options.port, options.host, () => {
+    console.log(`Server running at http://${options.host}:${options.port}/`);
+  });
+});
